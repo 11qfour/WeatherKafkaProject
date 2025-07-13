eb5fcf18a7c00fdd4b74d5fe2c994723ad15a3b0
11qfour <futurenature31@gmail.com>
2025-07-13 13:07:24 +0300

    init commit

---
+++ src/main/java/com/example_spring_kafka/spring_kafka_docker_int/SpringKafkaDockerIntApplication.java
@@ -0,0 +1,13 @@
+package com.example_spring_kafka.spring_kafka_docker_int;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaDockerIntApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKafkaDockerIntApplication.class, args);
+	}
+
+}
