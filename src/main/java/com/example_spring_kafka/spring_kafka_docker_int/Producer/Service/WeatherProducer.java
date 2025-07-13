fbeb23d02c96e21b0fa17cd8782bc7ba354a8afc
11qfour <futurenature31@gmail.com>
2025-07-13 21:21:59 +0300

    fix producer

--- src/main/java/com/example_spring_kafka/spring_kafka_docker_int/Producer/Service/WeatherProducer.java
+++ src/main/java/com/example_spring_kafka/spring_kafka_docker_int/Producer/Service/WeatherProducer.java
@@ -2,16 +2,20 @@ package com.example_spring_kafka.spring_kafka_docker_int.Producer.Service;
 
 import com.example_spring_kafka.spring_kafka_docker_int.Producer.POJO.WeatherData;
 import com.example_spring_kafka.spring_kafka_docker_int.Producer.WeatherGenerator;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class WeatherProducer {
-    private static final String TOPIC =" weather-topic";
-    private KafkaTemplate<String, WeatherData> kafkaTemplate;
+    private static final String TOPIC ="weather-topic";
+    private final KafkaTemplate<String, WeatherData> kafkaTemplate;
+    private final KafkaTemplate<String, String> kafkaTemplate2;
     private final WeatherGenerator weatherGenerator=new WeatherGenerator();
     public void writeMessage() {
+        kafkaTemplate2.send("weather-topic", "test message");
         WeatherData data = new WeatherData(
                 weatherGenerator.randomLocation(),
                 weatherGenerator.randomCondition(),
